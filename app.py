e581b977829a008e0deb921410077e2dd88fa46f
ar1001001 <ar3920@columbia.edu>
2020-05-15 21:58:41 -0400

    some changes

--- app.py
+++ app.py
@@ -2,21 +2,20 @@
 #import statements
 from flask import Flask, render_template, redirect, url_for
 
-#Flask app variable
+# Flask app variable
 app = Flask(__name__)
 
-#static route
+# static route
 @app.route("/")
 def hello():
     return render_template('index.html')
+
 
 @app.route("/<name>")
 def myBio(name):
     return f"Hello {name}!"
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("myBio", name = "maini"))
-#start the server
+
+# start the server
 if __name__ == "__main__":
-    app.run(debug = 'True')
+    app.run(debug='True')
